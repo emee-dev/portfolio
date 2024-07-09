398b762a58c5d1425d3d3683d0d1794c7f81921d
Ajike Emmanuel Jonah <emmanuelajike2000@gmail.com>
2024-07-10 00:02:23 +0100

    add rust client repo url

--- pages/index.js
+++ pages/index.js
@@ -5,68 +5,68 @@ import { Experience } from "../components/Experience";
 import { Grid } from "../components/Grid";
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>Emee - FullStack Developer</title>
-			</Head>
-			{/* <Cursor /> */}
+  return (
+    <>
+      <Head>
+        <title>Emee - FullStack Developer</title>
+      </Head>
+      {/* <Cursor /> */}
 
-			<Box py="115px" px={4} maxWidth={500} mx="auto">
-				<Grid
-					fluid
-					templateColumns="repeat(4, 1fr)"
-					mb={10}
-					alignItems="center"
-				>
-					<GridItem colSpan={1}>
-						<Avatar />
-					</GridItem>
-					<GridItem colSpan={3}>
-						<Box>
-							<Heading as="h1" size="lg">
-								{/* Maxime Bonhomme */}
-								Emmanuel Ajike
-							</Heading>
-							{/* <Text>Product-focused Fullstack Developer</Text> */}
-							<Text>Solution-Oriented Fullstack Developer</Text>
-							{/* <Link
+      <Box py="115px" px={4} maxWidth={500} mx="auto">
+        <Grid
+          fluid
+          templateColumns="repeat(4, 1fr)"
+          mb={10}
+          alignItems="center"
+        >
+          <GridItem colSpan={1}>
+            <Avatar />
+          </GridItem>
+          <GridItem colSpan={3}>
+            <Box>
+              <Heading as="h1" size="lg">
+                {/* Maxime Bonhomme */}
+                Emmanuel Ajike
+              </Heading>
+              {/* <Text>Product-focused Fullstack Developer</Text> */}
+              <Text>Solution-Oriented Fullstack Developer</Text>
+              {/* <Link
 								color="white"
 								opacity={0.5}
 								href="https://etherscan.io/address/0xff5fe6e0d3d48c90a66217dd4a7560a3ed8dacd2"
 							>
 								bonhomme.eth
 							</Link> */}
-							<Link
-								color="white"
-								opacity={0.5}
-								href="https://github.com/emee-dev"
-							>
-								emee-dev
-							</Link>
-						</Box>
-					</GridItem>
-				</Grid>
-				<Box mb={14}>
-					<Heading as="h2" size="md" mb={2}>
-						About
-					</Heading>
-					<Text>
-						{/* GM, I'm Max. I enjoy building dynamic, creative products from start
+              <Link
+                color="white"
+                opacity={0.5}
+                href="https://github.com/emee-dev"
+              >
+                emee-dev
+              </Link>
+            </Box>
+          </GridItem>
+        </Grid>
+        <Box mb={14}>
+          <Heading as="h2" size="md" mb={2}>
+            About
+          </Heading>
+          <Text>
+            {/* GM, I'm Max. I enjoy building dynamic, creative products from start
 						to finish. Focused on developing intuitive experiences that
 						constantly grow and improve based on user metrics. Always shipping. */}
-						Hello, I'm Emmanuel. I envy beautiful dynamic, creative products
-						which lead me to create, and publish even better products. I am
-						focused on developing intuitive experiences for my users. I love
-						indie hacking.
-					</Text>
-				</Box>
-				<Box mb={14}>
-					<Heading as="h2" size="md" mb={10}>
-						Work Experience
-					</Heading>
+            Hello, I'm Emmanuel. I envy beautiful dynamic, creative products
+            which lead me to create, and publish even better products. I am
+            focused on developing intuitive experiences for my users. I love
+            indie hacking.
+          </Text>
+        </Box>
+        <Box mb={14}>
+          <Heading as="h2" size="md" mb={10}>
+            Work Experience
+          </Heading>
 
-					{/* <Experience
+          {/* <Experience
 						href="https://iyk.app"
 						side="2022 - present"
 						title="Senior Frontend Engineer – IYK"
@@ -96,20 +96,20 @@ export default function Home() {
 						stack="React • Redux • CSSModules • Three.js"
 					/> */}
 
-					<Experience
-						// href="https://hacked.treblle.com/"
-						href="https://treblle-monorepo-docs.vercel.app/"
-						side="2023"
-						title="Treblle SDK – Hacktoberfest"
-						desc="Accounting for security, load balancing and data masking. I built an SDK that captures incoming API requests and responses, and sends that to Treblle servers."
-						stack="Express Js • Typescript • Nextra docs"
-					/>
+          <Experience
+            // href="https://hacked.treblle.com/"
+            href="https://treblle-monorepo-docs.vercel.app/"
+            side="2023"
+            title="Treblle SDK – Hacktoberfest"
+            desc="Accounting for security, load balancing and data masking. I built an SDK that captures incoming API requests and responses, and sends that to Treblle servers."
+            stack="Express Js • Typescript • Nextra docs"
+          />
 
-					<Heading as="h2" size="md" mt={14} mb={10}>
-						Side Projects
-					</Heading>
+          <Heading as="h2" size="md" mt={14} mb={10}>
+            Side Projects
+          </Heading>
 
-					{/* <Experience
+          {/* <Experience
 						image="/offline.gif"
 						side="Ongoing"
 						title="Offline Nostalgia"
@@ -132,28 +132,28 @@ export default function Home() {
 						stack="Solidity • ERC721 • Next.js"
 					/> */}
 
-					<Experience
-						// href="https://rustclient.dev/"
-						image="/offline.gif"
-						side="Ongoing"
-						title="Postman Alternative Http Client"
-						desc="Readable, diffable, local first. File based request collection. A free and simple and offline based Http client. Desktop and CLI supported."
-						stack="Rust • Tauri • Turborepo • file based • fully offline"
-					/>
+          <Experience
+            href="https://github.com/emee-dev/rustclient"
+            image="/offline.gif"
+            side="Ongoing"
+            title="Postman Http Client Alternative"
+            desc="Readable, diffable, local first. File based request collection. A free and simple and offline based Http client. Desktop and CLI supported."
+            stack="Rust • Tauri • Turborepo • file based • fully offline"
+          />
 
-					<Experience
-						image="/tamiko.svg"
-						side="Paused"
-						title="Kinde CLI"
-						desc="Interactive, easy to use CLI tool to manage your dashboard, and projects right in your terminal."
-						stack="Commander Js • Typescript • Kinde API"
-					/>
+          <Experience
+            image="/tamiko.svg"
+            side="Paused"
+            title="Kinde CLI"
+            desc="Interactive, easy to use CLI tool to manage your dashboard, and projects right in your terminal."
+            stack="Commander Js • Typescript • Kinde API"
+          />
 
-					<Heading as="h2" size="md" mt={14} mb={10}>
-						Links
-					</Heading>
+          <Heading as="h2" size="md" mt={14} mb={10}>
+            Links
+          </Heading>
 
-					{/* <Experience
+          {/* <Experience
 						side="Github"
 						title="@maximebonhomme"
 						href="https://github.com/maximebonhomme"
@@ -177,26 +177,26 @@ export default function Home() {
 						href="https://twitter.com/pixel_arts"
 						mb={4}
 					/> */}
-					<Experience
-						side="Github"
-						title="@emee-dev"
-						href="https://github.com/emee-dev"
-						mb={4}
-					/>
-					<Experience
-						side="Twitter"
-						title="@___emee_"
-						href="https://x.com/___emee_"
-						mb={4}
-					/>
-					<Experience
-						side="Credits"
-						title="@bonhomme"
-						href="https://github.com/maximebonhomme/bonhomme-2022"
-						mb={4}
-					/>
-				</Box>
-			</Box>
-		</>
-	);
+          <Experience
+            side="Github"
+            title="@emee-dev"
+            href="https://github.com/emee-dev"
+            mb={4}
+          />
+          <Experience
+            side="Twitter"
+            title="@___emee_"
+            href="https://x.com/___emee_"
+            mb={4}
+          />
+          <Experience
+            side="Credits"
+            title="@bonhomme"
+            href="https://github.com/maximebonhomme/bonhomme-2022"
+            mb={4}
+          />
+        </Box>
+      </Box>
+    </>
+  );
 }
